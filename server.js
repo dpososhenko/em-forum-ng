54b5a004e49ae5e44672c9a53fab12f530c37c84
dpososhenko <dmitriy.pososhenko@gmail.com>
2019-07-01 16:15:41 +0300

    EM-5 = proxy server fix

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/em-forum-ng'));
+app.use(express.static(__dirname + '/dist/em-forum-ng/'));
 
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+'/dist/em-forum-ng/index.html'));
 });
 
-app.listen(process.env.PORT || 4200);
+app.listen(process.env.PORT || 4200, () => {
+  console.log(`App running on PORT: ${process.env.PORT || 4200}`);
+});
